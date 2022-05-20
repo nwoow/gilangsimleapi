821f5193af91f1679139e47099aeea707dbfe162
Nilay Singh <nsnnilay@dt.ae>
2022-05-20 15:59:22 +0400

    updated syntax

--- Controllers/divide.go
+++ Controllers/divide.go
@@ -4,17 +4,20 @@ package controllers
 
 import (
 	models "DT/Models"
+	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 //Divide
 func Divide(c *gin.Context) {
-	var person models.Requests
-	c.BindJSON(&person)
-	divide_result_1 := person.A / person.B
-	divide_result_2 := person.B / person.A
-	c.JSON(200, gin.H{
+	fmt.Println("error")
+	var requests models.Requests
+	c.BindJSON(&requests)
+	divide_result_1 := requests.A / requests.B
+	divide_result_2 := requests.B / requests.A
+	c.JSON(http.StatusOK, gin.H{
 		"R1": divide_result_1,
 		"R2": divide_result_2, // cast it to string before showing
 	})
--- Router/router.go
+++ Router/router.go
@@ -11,12 +11,10 @@ func Router() {
 	Router := gin.Default()
 	r := Router.Group("/api")
 	r.POST("divide", controllers.Divide)
-	// r.GET("/logconfiglist", controllers.Logconfiglist) // new
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-
 	Router.Run(":8080")
 }
